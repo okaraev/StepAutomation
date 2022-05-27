cf6632b066aabe706a1980931fb3fff934e57ea7
Ogtay Garayev <okaraev@outlook.com>
2022-05-27 17:15:15 +0400

    removed Update function

--- Publish.ps1
+++ Publish.ps1
@@ -3,41 +3,6 @@ param(
     [parameter(mandatory)]
     [string]$Secret
 )
-
-function Update-File{
-    [CmdletBinding()]
-    param(
-        [parameter(Mandatory,Position=0)]
-        [System.IO.FileInfo[]]$Source,
-        [parameter(Mandatory,Position=1)]
-        [System.IO.FileInfo]$Destination
-    )
-    foreach($SourceItem in $Source){
-        $dstFile = $Destination
-        $needToCopy = $false
-        if(!$Destination.Name){
-            [System.IO.FileInfo]$dstFile = "$($Destination.Directory)\$($SourceItem.Name)"
-        }
-        if(!$SourceItem.Name){
-            throw "Source must be an File path array"
-        }
-        if(!$dstFile.Exists){
-            $needToCopy = $true
-        }
-        elseif((Get-FileHash -Path $SourceItem).Hash -ne (Get-FileHash $dstFile).Hash){
-            $needToCopy = $true
-        }
-        if($needToCopy){
-            Try{
-                Copy-Item -Path $SourceItem -Destination $dstFile -Force -ErrorAction Stop
-                Write-Verbose -Message "Updating $dstFile with $SourceItem"
-            }catch{
-                throw $_
-            }
-        }
-    }
-}
-
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Try{
     if((Get-PackageProvider).Name -notcontains "NuGet"){
@@ -60,12 +25,13 @@ Try{
 }
 if($null -ne $PSGalleryVersion){
     if($PSGalleryVersion -lt $localVersion){
+        $moduleDir = "C:\Program Files\WindowsPowershell\Modules\StepAutomation"
         Try{
-            if(!(Test-Path .\tmp)){
-                New-Item -ItemType Directory tmp | Out-Null
+            if(!(Test-Path $moduleDir)){
+                New-Item -ItemType Directory $moduleDir -Force | Out-Null
             }
-            Copy-Item .\StepAutomation.psd1,.\StepAutomation.psm1,.\Assemblies\ -Recurse -Destination .\tmp\ -Force
-            Publish-Module -Path .\tmp -NuGetApiKey $Secret -Repository PSGallery -Force -Confirm:$False -ErrorAction Stop
+            Copy-Item .\StepAutomation.psd1,.\StepAutomation.psm1,.\Assemblies\ -Recurse -Destination $moduleDir -Force
+            Publish-Module -Path $moduleDir -NuGetApiKey $Secret -Repository PSGallery -Force -Confirm:$False -ErrorAction Stop
             Write-Host Module Version Updated to $localVersion
         }catch{
             throw $_
