8ab32635f54e1e5d9a55ffe2e18b526a0d8cf35c
Ogtay Garayev <okaraev@outlook.com>
2022-05-27 17:06:32 +0400

    Fixed Publishing directory issue

--- Publish.ps1
+++ Publish.ps1
@@ -3,6 +3,41 @@ param(
     [parameter(mandatory)]
     [string]$Secret
 )
+
+function Update-File{
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory,Position=0)]
+        [System.IO.FileInfo[]]$Source,
+        [parameter(Mandatory,Position=1)]
+        [System.IO.FileInfo]$Destination
+    )
+    foreach($SourceItem in $Source){
+        $dstFile = $Destination
+        $needToCopy = $false
+        if(!$Destination.Name){
+            [System.IO.FileInfo]$dstFile = "$($Destination.Directory)\$($SourceItem.Name)"
+        }
+        if(!$SourceItem.Name){
+            throw "Source must be an File path array"
+        }
+        if(!$dstFile.Exists){
+            $needToCopy = $true
+        }
+        elseif((Get-FileHash -Path $SourceItem).Hash -ne (Get-FileHash $dstFile).Hash){
+            $needToCopy = $true
+        }
+        if($needToCopy){
+            Try{
+                Copy-Item -Path $SourceItem -Destination $dstFile -Force -ErrorAction Stop
+                Write-Verbose -Message "Updating $dstFile with $SourceItem"
+            }catch{
+                throw $_
+            }
+        }
+    }
+}
+
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Try{
     if((Get-PackageProvider).Name -notcontains "NuGet"){
@@ -26,8 +61,11 @@ Try{
 if($null -ne $PSGalleryVersion){
     if($PSGalleryVersion -lt $localVersion){
         Try{
-            write-host ${secret}
-            Publish-Module -Name StepAutomation -NuGetApiKey $Secret -Repository PSGallery -Force -Confirm:$False -ErrorAction Stop
+            if(!(Test-Path .\tmp)){
+                New-Item -ItemType Directory tmp | Out-Null
+            }
+            Copy-Item .\StepAutomation.psd1,.\StepAutomation.psm1,.\Assemblies\ -Recurse -Destination .\tmp\ -Force
+            Publish-Module -Path .\tmp -NuGetApiKey $Secret -Repository PSGallery -Force -Confirm:$False -ErrorAction Stop
             Write-Host Module Version Updated to $localVersion
         }catch{
             throw $_
